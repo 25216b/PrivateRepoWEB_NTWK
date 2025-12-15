08564aa7ee642fc149ed553d3d5a27afc0701f78
25216 <25216@ecam.be>
2025-12-15 21:59:32 +0100

    add changes to the page

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -38,21 +38,30 @@ export default function RootLayout({
         <Navbar></Navbar>
         <SectionId></SectionId>
         <CVEntry
-        sectionName="Education"
+          sectionName="Education"
           title="industrial computer science"
           company="HE2B-ESI"
           date="2025">
-            Graduated with an Engineering Diploma after completing extensive coursework and practical projects that strengthened my abilities in design, analysis, teamwork, and the application of engineering principles in real scenarios.
-          </CVEntry>
-          <CVEntry
+          Graduated with an Engineering Diploma after completing extensive coursework and practical projects that strengthened my abilities in design, analysis, teamwork, and the application of engineering principles in real scenarios.
+        </CVEntry>
+        <CVEntry
           sectionName="Experience"
           title="environemental enginner intern"
           company="PC2A -Lille,France"
           date="2025">
-            internship as an engineer in a national project where i had to develop ui and simple code for researcher and find solution on how to detectenvironement change
-            The challenge was that ai was prohibated so algorithmic and physics solution were the way to go
-          </CVEntry>
-          
+          internship as an engineer in a national project where i had to develop ui and simple code for researcher and find solution on how to detectenvironement change
+          The challenge was that ai was prohibated so algorithmic and physics solution were the way to go
+        </CVEntry>
+        <Contacts
+
+          sectionName="ok"
+          title="ok"
+          company="ok"
+          date="ok"
+        >
+
+        </Contacts>
+
         {children}
         <Footer></Footer>
       </body>
