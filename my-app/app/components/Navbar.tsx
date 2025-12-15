f9bb2fcb67cf9cd270d615526576bbce3fcea583
25216 <25216@ecam.be>
2025-12-16 00:22:08 +0100

    responsive design navbar

--- my-app/app/components/Navbar.tsx
+++ my-app/app/components/Navbar.tsx
@@ -8,24 +8,20 @@ type NavLinkProps={
 
 function NavLink(props: NavLinkProps){
     return (
-        <li className="text-white
-        hover:text-red-500 mx-[15px]">
+        <li className="text-white hover:text-red-500 mx-2 sm:mx-4 my-2 sm:my-0">
             <Link href={props.href}>{props.children}</Link>
         </li>
     )
 }
 
 export default function Navbar(){
-
     return (
-    <nav className="bg-black">
-        <ul className="list-none flex justify-center flex-wrap">
-            
+    <nav className="bg-black py-4">
+        <ul className="list-none flex flex-col sm:flex-row justify-center items-center">
             <NavLink href="#contact"> Contact </NavLink>
             <NavLink href="#Experience"> Experience </NavLink>
             <NavLink href="#Skills"> Skills </NavLink>
             <NavLink href="#About"> About </NavLink>
-            
         </ul>
     </nav>
     )
