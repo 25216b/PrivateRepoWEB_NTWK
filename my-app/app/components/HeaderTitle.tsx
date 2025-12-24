6c9b6680a8acd8b077b70feedb837757e54135ee
25216 <25216@ecam.be>
2025-12-24 04:43:57 +0100

    header

--- my-app/app/components/HeaderTitle.tsx
+++ my-app/app/components/HeaderTitle.tsx
@@ -5,7 +5,7 @@
     return (
         <header className="bg-gradient-to-br from-[#667eea] to-[#764ba2] text-white py-[60px] px-[20px] text-center">
             <h1 className="text-4xl font-bold">Welcome</h1>
-            <p className="mt-2 text-lg">This is a gradient header</p>
+            <p className="mt-2 text-lg">This is my CV</p>
         </header>
     )
 }
